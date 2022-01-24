35ee5d44f0b6e9c4dcc1c940cc41fff0c893537e
kondo0602 <kondo8363@gmail.com>
2022-01-24 23:21:55 +0900

    キャラクターの略称リストに全キャラクター追加

--- index.ts
+++ index.ts
@@ -46,15 +46,253 @@ async function formatCharacterName(nickName: string) {
 
   switch (nickName) {
     case "マリオ":
-    case "まりお":
       characterName = "01_マリオ";
       break;
+    case "ドンキーコング":
+      characterName = "02_ドンキーコング";
+      break;
+    case "リンク":
+      characterName = "03_リンク";
+      break;
+    case "サムス":
+      characterName = "04_サムス";
+      break;
+    case "ヨッシー":
+      characterName = "05_ヨッシー";
+      break;
+    case "カービィ":
+      characterName = "06_カービィ";
+      break;
+    case "フォックス":
+      characterName = "07_フォックス";
+      break;
+    case "ピカチュウ":
+      characterName = "08_ピカチュウ";
+      break;
     case "ルイージ":
-    case "るいーじ":
-      characterName = "02_ルイージ";
+      characterName = "09_ルイージ";
+      break;
+    case "ネス":
+      characterName = "10_ネス";
+      break;
+    case "キャプテン・ファルコン":
+      characterName = "11_キャプテン・ファルコン";
+      break;
+    case "プリン":
+      characterName = "12_プリン";
+      break;
+    case "ピーチ":
+      characterName = "13_ピーチ";
+      break;
+    case "クッパ":
+      characterName = "14_クッパ";
+      break;
+    case "アイスクライマー":
+      characterName = "15_アイスクライマー";
+      break;
+    case "シーク":
+      characterName = "16_シーク";
+      break;
+    case "ゼルダ":
+      characterName = "17_ゼルダ";
+      break;
+    case "ドクターマリオ":
+      characterName = "18_ドクターマリオ";
+      break;
+    case "ピチュー":
+      characterName = "19_ピチュー";
+      break;
+    case "ファルコ":
+      characterName = "20_ファルコ";
+      break;
+    case "ルキナ":
+      characterName = "21_ルキナ";
+      break;
+    case "こどもリンク":
+      characterName = "22_こどもリンク";
+      break;
+    case "ガノンドロフ":
+      characterName = "23_ガノンドロフ";
+      break;
+    case "ミュウツー":
+      characterName = "24_ミュウツー";
+      break;
+    case "ロイ":
+      characterName = "25_ロイ";
+      break;
+    case "Mr":
+      characterName = "26_Mr";
+      break;
+    case "メタナイト":
+      characterName = "27_メタナイト";
+      break;
+    case "ピット":
+      characterName = "28_ピット";
+      break;
+    case "ゼロスーツサムス":
+      characterName = "29_ゼロスーツサムス";
+      break;
+    case "ワリオ":
+      characterName = "30_ワリオ";
+      break;
+    case "スネーク":
+      characterName = "31_スネーク";
+      break;
+    case "アイク":
+      characterName = "32_アイク";
+      break;
+    case "ゼニガメ":
+      characterName = "33_ゼニガメ";
+      break;
+    case "フシギソウ":
+      characterName = "34_フシギソウ";
+      break;
+    case "リザードン":
+      characterName = "35_リザードン";
+      break;
+    case "ディディーコング":
+      characterName = "36_ディディーコング";
+      break;
+    case "リュカ":
+      characterName = "37_リュカ";
+      break;
+    case "ソニック":
+      characterName = "38_ソニック";
+      break;
+    case "デデデ":
+      characterName = "39_デデデ";
+      break;
+    case "ピクミン&オリマー":
+      characterName = "40_ピクミン&オリマー";
+      break;
+    case "ルカリオ":
+      characterName = "41_ルカリオ";
+      break;
+    case "ロボット":
+      characterName = "42_ロボット";
+      break;
+    case "トゥーンリンク":
+      characterName = "43_トゥーンリンク";
+      break;
+    case "ウルフ":
+      characterName = "44_ウルフ";
+      break;
+    case "むらびと":
+      characterName = "45_むらびと";
+      break;
+    case "ロックマン":
+      characterName = "46_ロックマン";
+      break;
+    case "Wii Fit トレーナー":
+      characterName = "47_Wii Fit トレーナー";
+      break;
+    case "ロゼッタ&チコ":
+      characterName = "48_ロゼッタ&チコ";
+      break;
+    case "リトル・マック":
+      characterName = "49_リトル・マック";
+      break;
+    case "ゲッコウガ":
+      characterName = "50_ゲッコウガ";
+      break;
+    case "格闘Mii":
+      characterName = "51_格闘Mii";
+      break;
+    case "剣術Mii":
+      characterName = "52_剣術Mii";
+      break;
+    case "射撃Mii":
+      characterName = "53_射撃Mii";
+      break;
+    case "パルテナ":
+      characterName = "54_パルテナ";
+      break;
+    case "パックマン":
+      characterName = "55_パックマン";
+      break;
+    case "ルフレ":
+      characterName = "56_ルフレ";
+      break;
+    case "シュルク":
+      characterName = "57_シュルク";
+      break;
+    case "クッパ Jr":
+      characterName = "58_クッパJr";
+      break;
+    case "ダックハント":
+      characterName = "59_ダックハント";
+      break;
+    case "リュウ":
+      characterName = "60_リュウ";
+      break;
+    case "クラウド":
+      characterName = "61_クラウド";
+      break;
+    case "カムイ":
+      characterName = "62_カムイ";
+      break;
+    case "ベヨネッタ":
+      characterName = "63_ベヨネッタ";
+      break;
+    case "インクリング":
+      characterName = "64_インクリング";
+      break;
+    case "リドリー":
+      characterName = "65_リドリー";
+      break;
+    case "シモン":
+      characterName = "66_シモン";
+      break;
+    case "キングクルール":
+      characterName = "67_キングクルール";
+      break;
+    case "しずえ":
+      characterName = "68_しずえ";
+      break;
+    case "ガオガエン":
+      characterName = "69_ガオガエン";
+      break;
+    case "パックンフラワー":
+      characterName = "70_パックンフラワー";
+      break;
+    case "ジョーカー":
+      characterName = "71_ジョーカー";
+      break;
+    case "勇者":
+      characterName = "72_勇者";
+      break;
+    case "バンジョー&カズーイ":
+      characterName = "73_バンジョー&カズーイ";
+      break;
+    case "テリー":
+      characterName = "74_テリー";
+      break;
+    case "ルフレ":
+      characterName = "75_ルフレ";
+      break;
+    case "ミェンミェン":
+      characterName = "76_ミェンミェン";
+      break;
+    case "スティーブ":
+      characterName = "77_スティーブ";
+      break;
+    case "セフィロス":
+      characterName = "78_セフィロス";
+      break;
+    case "ホムラ":
+      characterName = "79_ホムラ";
+      break;
+    case "ヒカリ":
+      characterName = "80_ヒカリ";
+      break;
+    case "カズヤ":
+      characterName = "81_カズヤ";
+      break;
+    case "ソラ":
+      characterName = "82_ソラ";
       break;
     default:
-      console.log("該当なし");
+      characterName = "該当なし"
   }
 
   return characterName;
